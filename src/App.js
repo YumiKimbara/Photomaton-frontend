7e1b06fa3abe779da2691d063ac229b170405b56
Yumi <a13158y@gmail.com>
2021-11-03 14:55:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import "../styles/Home.scss";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
