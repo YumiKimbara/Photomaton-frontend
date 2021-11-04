59c8003108b856e550cc96ae02c1ee901ee276a7
EricTW-91 <amperic05@gmail.com>
2021-11-04 14:51:07 -0700

    Profile page first commit

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import { dividerClasses } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            This is a profile 📄 
+        </div>
+    )
+}
+
+export default Profile
