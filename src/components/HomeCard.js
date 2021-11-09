917e9cc007ad04edd8db71cb86e2e826bb7fa8b9
Yumi <a13158y@gmail.com>
2021-11-08 16:49:47 -0800

    map posted data

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -5,7 +5,9 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Collapse from '@mui/material/Collapse';
 
-const HomeCard = () => {
+const HomeCard = ({postedData}) => {
+
+  console.log(postedData.text)
 
   const imgUrl = "https://media.istockphoto.com/photos/colorful-autumn-landscape-of-a-modern-city-by-the-river-picture-id896667814?k=20&m=896667814&s=612x612&w=0&h=ml5BaUKT14TnbZDwypUbgg-D8H0b6zj65QDIgAisVE0="
   const userImageUrl = "https://media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?k=20&m=1291208214&s=612x612&w=0&h=WbHbwklzP81iAWV0dPlQWuBLxnbqJFk81a9OZG6qvSM="
@@ -15,7 +17,7 @@ const HomeCard = () => {
      <Card sx={{ maxWidth: 345 }}>
       <CardHeader
         avatar={
-          <Avatar alt="User's picture" src={userImageUrl}>
+          <Avatar alt="User's picture" src={postedData.userImageUrl}>
             R
           </Avatar>
         }
@@ -25,14 +27,12 @@ const HomeCard = () => {
       <CardMedia
         component="img"
         height="194"
-        image={imgUrl}
+        image={postedData.imgUrl}
         alt="Posted image"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          {postedData.text}
         </Typography>
       </CardContent>
         <IconButton aria-label="add to favorites">
