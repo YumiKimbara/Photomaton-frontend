66a4baed06d359151731a796ffcc41cdfb992d68
Yumi <a13158y@gmail.com>
2021-11-12 16:43:10 -0800

    add style and logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,51 @@
+// import {useEffect} from 'react';
+// import { loadCSS } from 'fg-loadcss';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 // const theme = createTheme({
 //   components: {
 //     MuiIcon: {
-//       defaultProps: {
-//         // Replace the `material-icons` default value.
-//         baseClassName: 'material-icons',
+//       styleOverrides: {
+//         root: {
+//           // Match 24px = 3 * 2 + 1.125 * 16
+//           boxSizing: 'content-box',
+//           padding: 3,
+//           fontSize: '1.125rem',
+//           color: 'blue'
+//         },
 //       },
 //     },
 //   },
 // });
 
 
-
 const Footer = () => {
+
+//     useEffect(() => {
+//     const node = loadCSS(
+//       'https://use.fontawesome.com/releases/v5.14.0/css/all.css',
+//       // Inject before JSS
+//       document.querySelector('#font-awesome-css') || document.head.firstChild,
+//     );
+
+//     return () => {
+//       node.parentNode.removeChild(node);
+//     };
+//   }, []);
     
     return <>
     <div className="footerWrapper">
-    <HomeIcon baseClassName="icons" sx={{ fontSize: 35 }}/>
-    <SearchIcon sx={{ fontSize: 35 }}/>
-    <ControlPointIcon sx={{ fontSize: 35 }}/>
-    <FavoriteBorderIcon sx={{ fontSize: 35 }}/>
-    <AccountCircleIcon sx={{ fontSize: 35 }}/>
+    <HomeIcon className="icons" sx={{ fontSize: 35 }}/>
+    <SearchIcon className="icons" sx={{ fontSize: 35 }} />
+    <ControlPointIcon className="icons" sx={{ fontSize: 35 }}/>
+    <FavoriteBorderIcon className="icons" sx={{ fontSize: 35 }}/>
+    <AccountCircleIcon className="icons" sx={{ fontSize: 35 }}/>
     </div>
     </>
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+const logo = "/images/logo.png"
+
 const Header = () => {
     return <>
     <div className="headerWrapper">
+    <img className="logo" src={logo} alt="logo"/>
     <h1 className="neon">Photomaton</h1>
     </div>
     </>
