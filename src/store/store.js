ce4d3ba9700032d1bf6619c3f035d43a9b37e3f0
DodoBey <dogukanyigiter@gmail.com>
2021-11-30 02:58:03 -0800

    created redux for userAuth

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,29 @@
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import modalReducer from "../reducers/reducers.js"
+import { userLoginReducer, userRegisterReducer } from "../reducers/userReducers.js";
 
-export const store = createStore(modalReducer)
+const reducer = combineReducers({
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    modalReducer: modalReducer
+});
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage }
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+
+export default store;
+
+
