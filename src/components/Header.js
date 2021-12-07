298c3457045c66d135ee9093ee54051f9ddb3783
DodoBey <dogukanyigiter@gmail.com>
2021-12-06 22:25:19 -0800

    logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
-const logo = "/images/textLogo.png"
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { logout } from "../actions/userActions"
+
+const logo = "/images/textLogo.png";
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const logoutHandler = () => {
+        dispatch(logout());
+        navigate("/")
+    };
+
     return <>
-    <div className="headerWrapper">
-    <img className="logo" src={logo} alt="logo" className="textLogo"/>
-    </div>
+        <div className="headerWrapper">
+            <img className="logo" src={logo} alt="logo" className="textLogo" />
+            <button onClick={logoutHandler} className="headerIcon"><LogoutIcon style={{ fontSize: "20px" }} /></button>
+        </div>
     </>
 }
 
